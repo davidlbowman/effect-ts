a26123dd418c7b83ccb901520489e792a18c6a40
David L. Bowman <david@theinnovationlab.dev>
2024-08-04 19:40:36 -0500

    flatMap

---
+++ src/04_Pipelines/04_flatMap.ts
@@ -0,0 +1,18 @@
+import { Effect, pipe } from "effect"
+
+const applyDiscount = (
+	total: number,
+	discountRate: number,
+): Effect.Effect<number, Error> =>
+	discountRate === 0
+		? Effect.fail(new Error("Discount rate cannot be zero."))
+		: Effect.succeed(total - (total * discountRate) / 100)
+
+const fetchTransactionAmount = Effect.promise(() => Promise.resolve(100))
+
+const finalAmount = pipe(
+	fetchTransactionAmount,
+	Effect.flatMap((amount) => applyDiscount(amount, 5)),
+)
+
+Effect.runPromise(finalAmount).then(console.log)

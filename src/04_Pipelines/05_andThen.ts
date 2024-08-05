7a9051fcc4dc4965b9dce287ace5a4d49dcb3223
David L. Bowman <david@theinnovationlab.dev>
2024-08-04 19:45:23 -0500

    andThen

---
+++ src/04_Pipelines/05_andThen.ts
@@ -0,0 +1,20 @@
+import { Effect, pipe } from "effect"
+import { effect } from "effect/Layer"
+
+const applyDiscount = (
+	total: number,
+	discountRate: number,
+): Effect.Effect<number, Error> =>
+	discountRate === 0
+		? Effect.fail(new Error("Discount rate cannnot be zero"))
+		: Effect.succeed(total - (total * discountRate) / 100)
+
+const fetchTransactionAmount = Effect.promise(() => Promise.resolve(100))
+
+const result = pipe(
+	fetchTransactionAmount,
+	Effect.andThen((amount) => amount * 2),
+	Effect.andThen((amount) => applyDiscount(amount, 5)),
+)
+
+Effect.runPromise(result).then(console.log)
